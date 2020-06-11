62c023be2fd9605b2f5cabac4b6784a08411f224
diaphragm <diaphragm@9kv.org>
2020-06-11 13:25:02 +0900

    change pre specification

--- test/dev.js
+++ test/dev.js
@@ -28,11 +28,11 @@ a.push(`
 `)
 
 a.push(`
-<v-app>
-  <v-app-bar app="app" dense="dense" flat="flat">
-    <img src="@/assets/images/logo.svg" />
-    <img src="@/assets/images/hoge.svg" />
-  <v-content class="default-background-color">
+  <v-app>
+    <v-app-bar app="app" dense="dense" flat="flat">
+      <img src="@/assets/images/logo.svg" />
+      <img src="@/assets/images/hoge.svg" />
+    <v-content class="default-background-color">
 `)
 
 a.forEach(s => console.log(elh(s)))
